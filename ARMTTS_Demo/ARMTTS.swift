773c9eeb7beb2b4ff74397edf48e1111451668ce
Albert Grigoryan <albert_grigoryan@yahoo.com>
2022-11-04 13:06:16 +0100

    Minor change

--- ARMTTS_Demo/ARMTTS.swift
+++ ARMTTS_Demo/ARMTTS.swift
@@ -2,55 +2,34 @@ import Foundation
 import AVFoundation
 
 public class ARMTTS {
+    var apiUrl = "https://armtts.online:5555/preprocess"
     var rate: Float = 1.0
-    
-    var API_URL = "https://armtts.online:5555/synthesize"
+    let sampleRate = 22_000
+    let token: String
     
     private let fastSpeech2 = try! FastSpeech2(url: Bundle.main.url(forResource: "fastspeech2", withExtension: "tflite")!)
-    
     private let mbMelGan = try! MBMelGan(url: Bundle.main.url(forResource: "mb_melgan", withExtension: "tflite")!)
-
-    /// Mel spectrogram hop size
-    public let hopSize = 256
-    
-    /// Vocoder sample rate
-    let sampleRate = 22_000
-
     private let sampleBufferRenderSynchronizer = AVSampleBufferRenderSynchronizer()
-
     private let sampleBufferAudioRenderer = AVSampleBufferAudioRenderer()
 
-    init() {
+    init(token: String) {
+        self.token = token
         sampleBufferRenderSynchronizer.addRenderer(sampleBufferAudioRenderer)
     }
 
     public func speak(string: String) {
         let input_ids = process(string)
-        
         do {
             let melSpectrogram = try fastSpeech2.getMelSpectrogram(inputIds: input_ids, speedRatio: 2 - rate)
-            
             let data = try mbMelGan.getAudio(input: melSpectrogram)
             print(data)
-            
             let blockBuffer = try CMBlockBuffer(length: data.count)
             try data.withUnsafeBytes { try blockBuffer.replaceDataBytes(with: $0) }
-            
             let audioStreamBasicDescription = AudioStreamBasicDescription(mSampleRate: Float64(sampleRate), mFormatID: kAudioFormatLinearPCM, mFormatFlags: kAudioFormatFlagIsFloat, mBytesPerPacket: 4, mFramesPerPacket: 1, mBytesPerFrame: 4, mChannelsPerFrame: 1, mBitsPerChannel: 32, mReserved: 0)
-            
             let formatDescription = try CMFormatDescription(audioStreamBasicDescription: audioStreamBasicDescription)
-            
             let delay: TimeInterval = 1
-            
-            let sampleBuffer = try CMSampleBuffer(dataBuffer: blockBuffer,
-                                                  formatDescription: formatDescription,
-                                                  numSamples: data.count / 4,
-                                                  presentationTimeStamp: sampleBufferRenderSynchronizer.currentTime()
-                                                    + CMTime(seconds: delay, preferredTimescale: CMTimeScale(sampleRate)),
-                                                  packetDescriptions: [])
-            
+            let sampleBuffer = try CMSampleBuffer(dataBuffer: blockBuffer, formatDescription: formatDescription, numSamples: data.count / 4, presentationTimeStamp: sampleBufferRenderSynchronizer.currentTime() + CMTime(seconds: delay, preferredTimescale: CMTimeScale(sampleRate)), packetDescriptions: [])
             sampleBufferAudioRenderer.enqueue(sampleBuffer)
-            
             sampleBufferRenderSynchronizer.rate = 1
         }
         catch {
@@ -63,11 +42,8 @@ public class ARMTTS {
         let semaphore = DispatchSemaphore (value: 0)
 
         let parameters = [
-          [
-            "key": "text",
-            "value": text,
-            "type": "text"
-          ]] as [[String : Any]]
+          ["key": "text", "value": text, "type": "text"],
+          ["key": "api_key", "value": token, "type": "text"]] as [[String : Any]]
 
         let boundary = "Boundary-\(UUID().uuidString)"
         var body = ""
@@ -94,10 +70,8 @@ public class ARMTTS {
         }
         body += "--\(boundary)--\r\n";
         let postData = body.data(using: .utf8)
-
-        var request = URLRequest(url: URL(string: API_URL)!,timeoutInterval: Double.infinity)
+        var request = URLRequest(url: URL(string: apiUrl)!,timeoutInterval: Double.infinity)
         request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-
         request.httpMethod = "POST"
         request.httpBody = postData
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
@@ -106,17 +80,12 @@ public class ARMTTS {
             semaphore.signal()
             return
           }
-          print(String(data: data, encoding: .utf8)!)
-            
-            let json = try! JSONSerialization.jsonObject(with:data, options :[]) as! [String:Any]
-            sequence = json["ids"] as! [Int32]
-            
+          let json = try! JSONSerialization.jsonObject(with:data, options :[]) as! [String:Any]
+          sequence = json["ids"] as! [Int32]
           semaphore.signal()
         }
-
         task.resume()
         semaphore.wait()
-        
         return sequence
     }
     
--- ARMTTS_Demo/ContentView.swift
+++ ARMTTS_Demo/ContentView.swift
@@ -7,82 +7,26 @@ extension String {
 }
 
 struct ContentView: View {
-    @StateObject var tts = ARMTTS()
-    var voices = ["Gor"]
-    @State private var selectedVoice = "Gor"
+    @StateObject var tts = ARMTTS(token: "UPDATE_TOKEN_HERE")
     
-    @State var text = "Նոր Զելանդիա, 1950 թվական, մայրերը հանում են իրենց երեխաների մանկասայլակները հասարակական տրանսպորտից։"
+    
+    @State var text = "Ողջույն, իմ անունը Գոռ է։"
+    let maxLength = 300
+    let themeColor = Color(red: 0.26, green: 0.85, blue: 0.72)
     
     var body: some View {
-        ZStack {
-            VStack {
-                Image("Logo")
-                    .frame(height: 20)
-                Text("TTS ENGINE FOR THE ARMENIAN LANGUAGE")
-                    .frame(minHeight: 10)
-                    .font(Font.custom("Genos-Regular", size: 8))
-                    .foregroundColor(.white)
-                    .padding(.top, 7)
-                HStack {
-                    Text("Voice")
-                        .foregroundColor(.black)
-                        .padding(.leading)
-                    Spacer()
-                    Picker("Please choose a color", selection: $selectedVoice) {
-                        ForEach(voices, id: \.self) {
-                            Text($0)
-                        }
-                    }
-                }
-                .frame(height: 40)
-                .background(Color.white)
-                .cornerRadius(14)
-                .padding(.top, 10)
-                ZStack {
-                    VStack {
-                        TextEditor(text: $text)
-                            .onChange(of: text) { _ in
-                                if text.count > 300 {
-                                    text = text.trunc(length: 300)
-                                }
-                            }
-                        Text("\(text.count) / 300")
-                            .frame(maxWidth: .infinity, alignment: .trailing)
-                            .foregroundColor(text.count == 300 ? Color.red : Color.black)
-                            .padding(.trailing, 4)
-                            .padding(.bottom, 2)
-                    }
-                }
-                .frame(maxHeight: 220, alignment: .top)
-                .background(Color.white)
-                .cornerRadius(11)
-                .padding(.top)
-                Button(action: {
-                    tts.speak(string: text)
-                }, label: {
-                    HStack(spacing: 8) {
-                        Text("PLAY")
-                            .frame(width: 99, height: 32)
-                            .font(Font.system(size: 15, weight: .heavy))
-                            .foregroundColor(Color.black)
-                            .background(Color.white)
-                    }
-                    .frame(width: 99, height: 32)
-                    .cornerRadius(14)
-                })
-                .accentColor(Color.white)
-                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 4, x: 2, y: 4)
-                .padding(.top, 20)
-                Spacer()
+        VStack {
+            TextEditor(text: $text).onChange(of: text) { _ in
+                if text.count > maxLength { text = text.trunc(length: maxLength) }
             }
-            .padding()
+            Button {
+                tts.speak(string: text)
+            } label: {
+                Label("Speak", systemImage: "speaker.1")
+                    .foregroundColor(themeColor)
+            }
         }
-        .background(Color(red: 0.263, green: 0.851, blue: 0.722).ignoresSafeArea(edges: .all))
+        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+    
 }
